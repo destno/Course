4b16197df448efb7f601abf3b6734cc513615bb6
Destno <destno15@gmail.com>
2022-05-23 22:00:01 +0500

    Correction of errors over the second homework

--- Homework 2/functions.py
+++ Homework 2/functions.py
@@ -1,11 +1,12 @@
 '''
     1.  Напишите функцию, вычисляющую максимальное из трех чисел. Два вызова функции
 '''
-import re
-
 
 def max_int(num1, num2, num3):
-    return max([num1, num2, num3])
+    if all(isinstance(x, int) for x in [num1, num2, num3]):
+        return max([num1, num2, num3])
+    else:
+        return "Не все параметры цифры"
 
 print(max_int(1,2,5))
 print(max_int(543,8321,12))
@@ -22,10 +23,13 @@ print(reversed_string("test string"))
 '''
     3. Напишите функцию для вычисления факториала задаваемого числа. Два вызова функции.
 '''
-def factorial(int):
-    if int == 0:
-        return 1
-    return factorial(int-1) * int
+def factorial(num):
+    if isinstance(num, int): 
+        if num == 0:
+            return 1
+        return factorial(num-1) * num
+    else:
+        return "Не верный формат ввода"
 
 print(factorial(5))
 print(factorial(10))
@@ -34,7 +38,10 @@ print(factorial(10))
     4. Напишите функцию, удаляющую повторяющиеся элементы в списке. Три вызова функции.
 '''
 def remove_dublicate_from_list(lis):
-    return list(set(lis))
+    if isinstance(lis, list):
+        return list(set(lis))
+    else:
+        return "На вход подан не лист"
 
 print(remove_dublicate_from_list(["etst", "test", "list", "test"]))
 print(remove_dublicate_from_list(["3", "2", "1", "3"]))
@@ -47,9 +54,7 @@ print(remove_dublicate_from_list(["3", "3", "test", "test"]))
 '''
 
 def palindrome(string):
-    reversed_string = string[::-1]
-    boole = string.upper() == reversed_string.upper()
-    return boole
+    return string.upper() == string[::-1].upper()
 
 print(palindrome("Level"))
 print(palindrome("Up"))
@@ -60,14 +65,17 @@ print(palindrome("Up"))
 '''
 
 def dec_max_int(func):
-    def squaring(*args, **kwargs):
-        res = func(*args, **kwargs)
+    def squaring(*args):
+        res = func(*args)
         return res**2
     return squaring
 
 @dec_max_int
 def max_int(num1, num2, num3):
-    return max([num1, num2, num3])
+    if all(isinstance(x, int) for x in [num1, num2, num3]):
+        return max([num1, num2, num3])
+    else:
+        return "Не все параметры цифры"
 
 print(max_int(1,2,5))
 print(max_int(543,8321,12))
@@ -76,8 +84,8 @@ print(max_int(543,8321,12))
     7. Декорируйте функцию из 9 задания таким образом, чтобы возвращалась строка в верхнем регистре.
 '''
 def upper_case(func):
-    def upper(*args, **kwargs):
-        str = func(*args, **kwargs)
+    def upper(*args):
+        str = func(*args)
         return str.upper()
     return upper
 
